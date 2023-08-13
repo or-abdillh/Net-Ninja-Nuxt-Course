c54bfc9307d04c03d6b23082c93b1d599c26afd8
or-abdillh <hans.abdillh05@gmail.com>
2023-08-13 21:32:31 +0800

    Add Tailwind config file

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+export default {
+    theme: {},
+    plugins: [],
+    content: [
+        `./components/**/*.{vue,js,ts}`,
+        `./layouts/**/*.vue`,
+        `./pages/**/*.vue`,
+        `./composables/**/*.{js,ts}`,
+        `./plugins/**/*.{js,ts}`,
+        `./utils/**/*.{js,ts}`,
+        `./App.{js,ts,vue}`,
+        `./app.{js,ts,vue}`,
+        `./Error.{js,ts,vue}`,
+        `./error.{js,ts,vue}`,
+        `./app.config.{js,ts}`
+    ]
+}

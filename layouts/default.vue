ee9d569085e39cc9ec00283e3ee0c4e5b71ba4b1
or-abdillh <hans.abdillh05@gmail.com>
2023-08-13 21:03:14 +0800

    Layouts

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">Dojo</NuxtLink>
+            <ul>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+                <li><NuxtLink to="/about">About Us</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <slot />
+    </main>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,8 @@
 <template>
-
-  <header>
-    <nav>
-      <NuxtLink to="/">Dojo</NuxtLink>
-      <ul>
-        <li><NuxtLink to="/products">Products</NuxtLink></li>
-        <li><NuxtLink to="/about">About Us</NuxtLink></li>
-      </ul>
-    </nav>
-  </header>
-
-  <section>
-    <h2>Dojo - Home Page</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A mollitia ad quasi temporibus non corrupti, inventore architecto, enim distinctio quis voluptas. Iure neque similique animi?</p>
-  </section>
+  <NuxtLayout name="default">
+    <section>
+      <h2>Dojo - Home Page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A mollitia ad quasi temporibus non corrupti, inventore architecto, enim distinctio quis voluptas. Iure neque similique animi?</p>
+    </section>
+  </NuxtLayout>
 </template>

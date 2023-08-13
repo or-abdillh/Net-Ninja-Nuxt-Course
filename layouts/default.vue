cfae96b7b01cff1533f83e9a6886d0d124c875dd
or-abdillh <hans.abdillh05@gmail.com>
2023-08-13 21:27:26 +0800

    Install Tailwind module

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,23 @@
 <template>
-    <header>
-        <nav>
+    <header class="sticky top-0 left-0 right-0">
+        <nav class="bg-white shadow-sm flex justify-between px-8 py-6">
             <NuxtLink to="/">Dojo</NuxtLink>
-            <ul>
+            <ul class="flex gap-4">
                 <li><NuxtLink to="/products">Products</NuxtLink></li>
                 <li><NuxtLink to="/about">About Us</NuxtLink></li>
             </ul>
         </nav>
     </header>
 
-    <main>
+    <main class="p-8">
         <slot />
     </main>
 </template>
+
+<style scoped>
+
+.router-link-exact-active {
+    @apply text-green-600 font-medium;
+}
+
+</style>

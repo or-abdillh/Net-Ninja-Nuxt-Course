0203086ce6cda1cdfbc55d31943b23b7e0c0ecd8
or-abdillh <hans.abdillh05@gmail.com>
2023-08-14 14:11:21 +0800

    Fetching products using useFetch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="sticky top-0 left-0 right-0">
         <nav class="bg-white shadow-sm flex justify-between px-8 py-6">
-            <NuxtLink to="/">Dojo</NuxtLink>
+            <NuxtLink class="font-semibold text-lg" to="/">Nuxt Dojo Merch</NuxtLink>
             <ul class="flex gap-4">
                 <li><NuxtLink to="/products">Products</NuxtLink></li>
                 <li><NuxtLink to="/about">About Us</NuxtLink></li>
@@ -13,11 +13,3 @@
         <slot />
     </main>
 </template>
-
-<style scoped>
-
-.router-link-exact-active {
-    @apply text-green-600 font-medium;
-}
-
-</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,8 +1,38 @@
 <template>
     <NuxtLayout name="default">
-        <section>
-            <h2>Products - Dojo</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt dicta temporibus iure placeat amet repellat reiciendis aut earum quidem, ab magni debitis recusandae facere, repudiandae fuga consectetur, expedita in harum?</p>
+
+        <!-- title -->
+        <section class="mb-8">
+            <h2 class="text-green-500 text-xl">Products - Dojo Merch</h2>
+            <p class="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita cumque iusto, dolore alias praesentium odit nostrum! Commodi tempore adipisci aut, labore aliquam ipsam tempora error, minima, enim atque quae quod.</p>
         </section>
+        <!-- end of title -->
+
+        <!-- products counter -->
+        <section class="mb-5 text-right">
+            <p class="text-gray-600">Showing {{ products.length }} products</p>
+        </section>
+        <!-- end of products counter -->
+
+        <!-- list of cards -->
+        <section class="grid grid-cols-4 gap-4">
+            <template v-for="product in products" :key="product?.id">
+                <!-- card -->
+                <div class="bg-white p-6 rounded-lg shadow-sm flex flex-col justify-between duration-300 border border-gray-100 hover:border-green-500">
+                    <div>
+                        <small>Product</small>
+                        <h1 class="mb-3 text-lg">{{ product?.title }}</h1>
+                    </div>
+                    <NuxtLink class="text-green-500 text-sm" :to="`/products/${ product?.id }`">Detail Product</NuxtLink>
+                </div>
+            </template>
+        </section>
+        <!-- end of list of cards -->
     </NuxtLayout>
 </template>
+
+<script setup>
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+</script>

92ef6f78ed502a52bc1350c349ca0745e5beef1d
or-abdillh <hans.abdillh05@gmail.com>
2023-08-14 21:57:26 +0800

    Error Page

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- wrapper -->
+    <section class="min-h-screen w-full bg-gray-50 grid place-items-center text-center">
+        <section class="p-12 bg-white md:w-4/12 rounded border">
+            <h1 class="text-7xl text-green-500 mb-8">{{ error?.statusCode }}</h1>
+            <h2 class="text-xl mb-1">Ooops...</h2>
+            <p class="mb-8">{{ error?.message }}</p>
+            <button @click="clearErrorPage" class="btn btn-primary px-6">Back To Home</button>
+        </section>
+    </section>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    error: {
+        type: Object
+    }
+})
+
+const clearErrorPage = () => clearError({ redirect: '/' })
+
+</script>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -7,10 +7,18 @@
 
 <script setup>
 
+// get param ID
 const { id } = useRoute().params
 
+// generate URI
 const URI = `https://fakestoreapi.com/products/${ id }`
 
+// fetching detail product
 const { data: product } = await useFetch(URI, { key: URI })
+
+// empty fetch result
+if ( !product.value ) {
+    throw createError({ statusCode: '404', message: `Product detail with ID ${ id } not found` })
+}
 
 </script>
